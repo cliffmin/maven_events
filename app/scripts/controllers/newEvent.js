1837b95c7a5c71a65978ea7deef204a659a1d0e4
Cliff <cliffmin@gmail.com>
2014-07-30 06:15:09 -0700

    last changes

--- app/scripts/controllers/newEvent.js
+++ app/scripts/controllers/newEvent.js
@@ -5,14 +5,35 @@
 'use strict';
 
 angular.module('mavenApp')
-    .controller('NewEventCtrl', function ($scope, $location) {
+    .controller('NewEventCtrl', function ($scope, $location, $http) {
         $scope.login = {
             "firstName": "Johnathon",
             "lastName": "Johnson"
         };
         $scope.addEvent = function (event, login) {
-            event.source = event.source + ' Person name:' + login.firstName +' '+login.lastName;
-            alert(JSON.stringify(event));//need to verify data
-            $location.path("/show/"+event.event_number);//need to add a success flag upon saving to the database
+            event = JSON.stringify(event)
+            event.source = event.source + ' Person name:' + login.firstName + ' ' + login.lastName;
+            alert(event);//need to verify data
+            $location.path("/show/" + event.event_number);//need to add a success flag upon saving to the database
+            $http({
+                method: 'POST',
+                url: 'http://winp179:40080/maven-events/api/v1/events',
+                data: event,  // pass in data as strings
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+            })
+                .success(function (data) {
+                    console.log(data);
+
+                    if (!data.success) {
+                        // if not successful, bind errors to error variables
+                        $scope.errorName = data.errors.name;
+                        $scope.errorSuperhero = data.errors.superheroAlias;
+                        alert("error");
+                    } else {
+                        // if successful, bind success message to message
+                        $scope.message = data.message;
+                        alert("success");
+                    }
+                });
         }
     });
